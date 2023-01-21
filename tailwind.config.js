4c96da66e5064c195784054fce4d4b9131a9872d
Andres Gutierrez Ramirez <andres.gutierrez@pivotree.com>
2023-01-22 00:20:07 +0100

    Add Tailwind initial settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,14 +6,14 @@ module.exports = {
       colors: {
         cyan: {
           300: 'hsl(179, 47%, 52%)',
-          500: 'hsl(179, 62%, 43%)'
+          500: 'hsl(179, 62%, 43%)',
         },
         yellow: {
           300: 'hsl(71, 73%, 54%)',
         },
         gray: {
-          300: "hsl(204, 43%, 93%)",
-          500: "hsl(218, 22%, 67%)",
+          300: 'hsl(204, 43%, 93%)',
+          500: 'hsl(218, 22%, 67%)',
         },
         white: 'hsl(0, 0%, 100%)',
       },
@@ -21,9 +21,13 @@ module.exports = {
         sans: ['Karla', 'sans-serif'],
       },
       fontSize: {
-        sm: '0.6875rem',
+        sm: '0.875rem',
         base: 'var(--fs-400)',
-        xl: 'var(--fs-600)'
+        xl: 'var(--fs-500)',
+        '2xl': 'var(--fs-600)',
+      },
+      boxShadow: {
+        DEFAULT: '0 0 1rem rgb(0 0 0 / 25%);',
       },
     },
   },
